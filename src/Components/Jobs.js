d1b1fce27e470aa3ccd9ee2549c03d6503d6b5ba
fridah mbithe <fridahmbithe45@gmail.com>
2024-07-02 12:49:01 +0300

    updated jobs section to be responsive on all screens

--- src/Components/Jobs.js
+++ src/Components/Jobs.js
@@ -2,12 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 
 
-
 const JobsPage = styled.div`
   background-color: #F4FEFE;
-  margin: 0 ;
-  padding: 4rem;
-`
+  margin: 0;
+  padding: 2rem; // Reduce padding for small screens
+
+  @media (min-width: 768px) {
+    padding: 4rem; // Increase padding for medium and larger screens
+  }
+`;
+
 const JobsContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -20,38 +24,62 @@ const JobCard = styled.div`
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   display: flex;
   align-items: center;
-  height: 220px;
-  padding: 8px
+  height: 180px; // Reduce height for small screens
+  padding: 8px;
+
+  @media (min-width: 768px) {
+    height: 220px; // Increase height for medium and larger screens
+  }
 `;
 
 const JobImage = styled.img`
-  width: 100px;
-  height: 100px;
+  width: 80px; // Reduce size for small screens
+  height: 80px;
   object-fit: fit;
   border-radius: 50%;
 
+  @media (min-width: 768px) {
+    width: 100px; // Increase size for medium and larger screens
+    height: 100px;
+  }
 `;
 
 const JobDetails = styled.div`
   flex-grow: 1;
-  padding: 1rem;
+  padding: 0.5rem; // Reduce padding for small screens
+  
+  @media (min-width: 768px) {
+    padding: 1rem; // Increase padding for medium and larger screens
+  }
 `;
 
 const CompanyName = styled.h3`
-  margin-bottom: 0.5rem;
+  margin-bottom: 0.3rem;
   color: #FE9900;
   font-weight: 600;
+  font-size: 0.6rem; // Reduce font size for small screens
+  
+  @media (min-width: 768px) {
+    font-size: 1.2rem; // Increase font size for medium and larger screens
+  }
 `;
 
 const JobDescription = styled.p`
   margin-bottom: 0.2rem;
+  font-size: 0.5rem; // Reduce font size for small screens
+  
+  @media (min-width: 768px) {
+    font-size: 1rem; // Increase font size for medium and larger screens
+  }
 `;
+
 const SkillList = styled.div`
   display: flex;
   flex-wrap: wrap;
   gap: 0.5rem;
   margin-bottom: 0.4rem;
 `;
+
 const Skill = styled.span`
   background-color: #f0f0f0;
   padding: 0.25rem 0.25rem;
@@ -62,13 +90,16 @@ const Skill = styled.span`
 const ReadMoreLink = styled.a`
   color: #007bff;
   text-decoration: none;
+  font-size: 0.4rem;
+  margin-bottom: 0.2rem;
 
   &:hover {
     text-decoration: underline;
   }
+      @media (min-width: 768px) {
+    font-size: 1rem; // Increase font size for medium and larger screens
+  }
 `;
-
-
 const jobs = [
   {
     id: 1,
